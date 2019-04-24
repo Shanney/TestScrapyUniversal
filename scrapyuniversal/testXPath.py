2045f57f00a71f0ca18b24e86da248cfb0b42307
xiangnan <49220598@qq.com>
2019-04-24 21:53:17 +0800

    directly parsing html file using custom parser to parse successfully there should be tbody between dom table and tr any dom index comes from 1 can use double slashes instead of tbody between table and tr

--- scrapyuniversal/testXPath.py
+++ scrapyuniversal/testXPath.py
@@ -1,4 +1,4 @@
-
+from lxml import etree
 
 class TestXPath():
 
@@ -8,7 +8,15 @@ class TestXPath():
     def test(self):
         with open('/Users/xiangnan/test.html') as file_object:
             contents = file_object.read()
-        print(contents)
+        # print(contents)
+
+        parser = etree.HTMLParser(encoding="utf-8")
+        html = etree.parse('/Users/xiangnan/test.html', parser=parser)
+        # print(type(html))
+        # a = etree.tostring(html, pretty_print=True)
+        # print(a)
+        result = html.xpath("//div[@id='_container_baseInfo']/table[2]/tbody/tr[10]/td[2]/text()")
+        print(result)
 
 
 if __name__ == '__main__':
