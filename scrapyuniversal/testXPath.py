d7ad9fd96b1646cf7cb8971096f14d307e988c79
xiangnan <49220598@qq.com>
2019-04-25 17:06:01 +0800

    '

--- scrapyuniversal/testXPath.py
+++ scrapyuniversal/testXPath.py
@@ -6,16 +6,18 @@ class TestXPath():
         pass
 
     def test(self):
-        with open('/Users/xiangnan/test.html') as file_object:
+        with open('test.html', encoding='UTF-8') as file_object:
             contents = file_object.read()
         # print(contents)
 
         parser = etree.HTMLParser(encoding="utf-8")
-        html = etree.parse('/Users/xiangnan/test.html', parser=parser)
+        html = etree.parse('test.html', parser=parser)
         # print(type(html))
         # a = etree.tostring(html, pretty_print=True)
         # print(a)
-        result = html.xpath("//div[@id='_container_baseInfo']/table[2]/tbody/tr[10]/td[2]/text()")
+        # //div[@id='_container_baseInfo']/table[2]/tbody/tr[10]/td[2]/text()
+        # https://www.cnblogs.com/MrFiona/p/5954084.html
+        result = html.xpath("//div[ @ id = '_container_baseInfo'] / table[2] / tbody / tr[1] / td[4] / div/text/text()")
         print(result)
 
 
